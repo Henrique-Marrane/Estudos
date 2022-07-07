afda5a558663ac1476d7f2479c6ec47aa075c4c1
Marrane <49756227+Henrique-Marrane@users.noreply.github.com>
2022-07-07 11:13:44 -0300

    Create ex002.py

---
+++ python/PythonExercicios/ex002.py
@@ -0,0 +1,8 @@
+#Neste programa vc deve pedir para o usuario informa dois numero para o programa somar os dois e mostra o resultado
+print('-------------------')
+n1 = input('Digite o primeiro numero: ')
+n2 = input('Digite o segundo numero: ')
+print('A soma dos numeros', n1 + ' e', n2 + ' é', int(n1)+int(n2))
+print('A soma dos numeros {}'.format(int(n1)+int(n2)))
+print('-------------------')
+print('Fim do progroma!')
