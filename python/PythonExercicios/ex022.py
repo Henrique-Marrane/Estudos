938520c42d415a1ad8e7f55c620cace6843e892f
Marrane <49756227+Henrique-Marrane@users.noreply.github.com>
2022-07-11 01:38:13 -0400

    finalizando-modulo

--- python/PythonExercicios/ex022.py
+++ python/PythonExercicios/ex022.py
@@ -1,6 +1,14 @@
-frase = 'Curso em Video Python'
-print(frase[9::3])
-print(len(frase))
-print(frase.count('o'))
-print(frase.find('deo'))
-print('Curso' in frase)
+# Crie um programa que leia o nome completo de uma pessoa e mostre:
+#
+# – O nome com todas as letras maiúsculas e minúsculas.
+#
+# – Quantas letras ao todo sem considerar espaços.
+#
+# – Quantas letras tem o primeiro nome.
+
+nome = str(input('Digite seu nome: ')).strip()
+print('Analisando seu nome...')
+print('Seu nome em maiusculo e {}'.format(nome.upper()))
+print('Seu nome em minusculo e {}'.format(nome.lower()))
+print('Seu nome tem ao toto {} letras.'.format(len(nome) - nome.count(' ')))
+print('Seu primeiro nome tem {} letras.'.format(nome.find(' ')))
